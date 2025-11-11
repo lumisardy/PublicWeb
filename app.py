a6e5eb8b96cc18529852a18216be803e93c1d6b4
lumisardy <86325886+lumisardy@users.noreply.github.com>
2025-11-11 23:15:16 +0100

    Refactor Flask app structure and routes

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+# Ruta absoluta del directorio actual
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(
+    __name__,
+    static_folder=os.path.join(BASE_DIR, "static"),
+    template_folder=os.path.join(BASE_DIR, "templates")
+)
 
 @app.route("/")
 def index():
-    return render_template("Formulario.html")
-
-@app.route("/misvacaciones")
-def vacaciones():
     return render_template("MisVacaciones.html")
+
+@app.route("/formulario")
+def vacaciones():
+    return render_template("formulario.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
